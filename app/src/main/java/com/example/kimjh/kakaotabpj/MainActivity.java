289688049b82453bf097804195b321a47f56dcd6
kimjeongho <jeongho9496@daum.net>
2016-02-22 17:22:11 +0900

    ver3.0

--- app/src/main/java/com/example/kimjh/kakaotabpj/MainActivity.java
+++ app/src/main/java/com/example/kimjh/kakaotabpj/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
 import com.example.kimjh.kakaotabpj.tabview.Tabview_channel;
 import com.example.kimjh.kakaotabpj.tabview.Tabview_chatt;
@@ -16,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     TabLayout tabLayout;
     ViewPager pager;
     MyPagerAdapter mAdapter;
+    TextView a;
 
 
     @Override
